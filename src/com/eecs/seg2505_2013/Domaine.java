d204c17e2353b704ee2977eb5554089e304fd24d
Robert Laganiere <robert@laganiere.name>
2013-11-15 08:48:34 -0500

    ajout de domaine

--- src/com/eecs/seg2505_2013/Domaine.java
+++ src/com/eecs/seg2505_2013/Domaine.java
@@ -8,6 +8,7 @@ public class Domaine implements Comparable<Domaine> {
 	private Domaine superDomaine;
 	private List<Domaine> sousdomaines;
 
+     // Construction d'un sous-domaine
 	public Domaine(String nom, Domaine superDomaine)
 	{
 		this.nom = nom;
@@ -16,6 +17,7 @@ public class Domaine implements Comparable<Domaine> {
 		superDomaine.getSousDomaine().add(this);
 	}
 
+     // Construction d'un domaine
 	public Domaine(String nom)
 	{
 
@@ -24,60 +26,81 @@ public class Domaine implements Comparable<Domaine> {
 		this.sousdomaines = new ArrayList<Domaine>(3);
 	}
 
+     // Ajout d'un sous-domaine
 	public boolean addSousDomaine(String nom)
 	{
+		if (isSousDomaine()) return false;
+
 		sousdomaines.add(new Domaine(nom, this));
 		return true;
 	}
 	
+      
+     // Ajout d'un sous-domaine
 	public boolean addSousDomaine(Domaine sousDomaineToAdd) {
+
+		if (isSousDomaine()) return false;
+
 		sousdomaines.add(sousDomaineToAdd);
 		return true;
 	}
-		
+
+     // Retourne le super-domaine 
+     // (null si c'est un super-domaine)		
 	public Domaine getSuperDomaine(){
 
 		return superDomaine;
 	}
-	
+
+     // retourne le nom d'un domaine	
 	public String getNom(){
 
 		return nom;
 	}
 	
+     // donne un nom au domaine	
 	public boolean setNom(String nom){
 		
 		this.nom = nom;	
 		return true;
 	}
 		
+     // retire un sous-domaine
 	public boolean remove(Domaine domaine){
+
+		if (isSousDomaine()) return false;
 
 		return sousdomaines.remove(domaine);
 	}
 
+     // conversion a une chaine de caracteres
 	public String toString() {
 		if(isSousDomaine()) {
-			return this.superDomaine.getNom() + " :\n" + nom;
+			return this.superDomaine.getNom() 
+                            + " :\n" + nom;
 		} else {
 			return nom;
 		}
 	}
 
+     // obtenir la liste des sous-domaines
 	public List<Domaine> getSousDomaine()
 	{
 		return sousdomaines;
 	}
 
+     // egalite entre domaines
 	public boolean equals(Object o){
 
 		return toString().equals(o.toString());
 	}
 	
+     // est-ce un sous-domaine
 	public boolean isSousDomaine() {
 		return superDomaine != null;
 	}
 
+     // comparaison entre domaines
 	public int compareTo(Domaine other) {
 		return toString().compareTo(other.toString());
 	}
