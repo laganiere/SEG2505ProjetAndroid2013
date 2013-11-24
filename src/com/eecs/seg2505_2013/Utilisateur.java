efe53104dc4ec32e7f92301f8f054f282b65fdad
nicolas.primeau@gmail.com <nicolas.primeau@gmail.com>
2013-11-23 21:29:34 -0500

    changed utilisateurs from arrays to lists

--- src/com/eecs/seg2505_2013/Utilisateur.java
+++ src/com/eecs/seg2505_2013/Utilisateur.java
@@ -1,20 +1,15 @@
 package com.eecs.seg2505_2013;
 
-//import java.util.Date;
-
-//Needs moar question and reponse
+import java.util.*;
 
 public class Utilisateur {
 	private String nom;
 	private int reputation;
-	private Domaine domaines[];
-	/*private Question questions[];
-	private Reponse reponses[];*/
+	private LinkedList<Domaine> domaines;
+
 	
 	public Utilisateur(){
-		domaines = new Domaine[10]; 
-		/*questions = new Question[10];
-		reponses = new Reponse[10];*/
+		domaines = new LinkedList<Domaine>(); 
 	}
 	
 	public Utilisateur(String nom){
@@ -22,43 +17,6 @@ public class Utilisateur {
 		this.nom = nom;
 	}
 	
-	public Utilisateur(String nom, int reputation){
-		this(nom);
-		this.reputation = reputation;
-	}
-	
-	//Access methods pour question
-	/*
-	public void addQuestion(Question question){this.<Question>addElement(question); }
-	
-	public void addQuestion(String text, Date date,Question.State etat, Domaine domaine, Utilisateur utilisateur){this.addQuestion(new Question(text,date,etat,domaine,utilisateur)); }
-	
-	public Question[] getQuestions(){ return this.<Question>getElements(new Question(null, null, null, null, null)); }
-	
-	public Question getQuestion(int index) throws IllegalArgumentException{ return this.<Question>getElement(index, new Question(null,null,null,null,null)); }
-	
-	public boolean hasQuestion(Question question){ return this.<Question>hasElement(question); }
-	
-	public int getIndexOfQuestion(Question question){  return this.<Question>getIndexOf(question); }
-	
-	public void removeQuestion(Question question){ this.<Question>removeElement(question); }
-	*/
-	//Access methods pour Reponse
-	/*
-	public void addReponse(Reponse reponse){this.<Reponse>addElement(reponse); }
-	
-	public void addResponse(String text, Date date, int qualite, Question question){ this.addReponse(new Reponse(text,date,qualite,question)); }
-	
-	public Reponse[] getReponses(){ return this.<Reponse>getElements(new Reponse(null)); }
-	
-	public Reponse getResponse(int index) throws IllegalArgumentException { return this.<Reponse>getElement(index, new Reponse(null)); }
-	
-	public boolean hasReponse(Reponse reponse){ return this.<Reponse>hasElement(reponse); }
-	
-	public int getIndexOfReponse(Reponse reponse){ return this.<Reponse>getIndexOf(reponse); }
-	
-	public void removeReponse(Reponse reponse){ this.<Reponse>removeElement(reponse); }
-	*/
 	//Access methods pour Domaine
 	
 	public void addDomaine(Domaine domaine){ this.<Domaine>addElement(domaine); }
@@ -104,130 +62,59 @@ public class Utilisateur {
 	
 	public String toString(){
 		String ret  = "Nom: "+ this.getNom() + "\n";
-		ret = ret + "Domaines: " + domaines[0].getNom();
-		for(int i=1;i<domaines.length &&domaines[i] != null;i++){
-			ret += ", " + domaines[i].getNom();
+		ret = ret + "Domaines: " + domaines.get(0).getNom();
+		
+		for(int i=1;i<domaines.size();i++){
+			ret += ", " + domaines.get(i).getNom();
 		}
-		/*ret += "\n"; 
-		ret = "Questions: ";
-		for(int i=1;i<questions.length &&questions[i] != null;i++){
-			ret += questions[i].getText() + "\n\n";
-		}
-		ret = "Reponse: ";
-		for(int i=1;i<reponses.length &&reponses[i] != null;i++){
-			ret += reponses[i].getText() + "\n\n";
-		}*/
 		
 		return ret;
 	}
 	
 	//Private Generic Methods
 	
-	private <E> void makeBiggerArray(E tst){
-		Object[] array = null;
-		if(tst instanceof Domaine){
-			array = domaines;
-		}/*else if(tst  instanceof Question){
-			array = questions;
-		}else if(tst instanceof Reponse){
-			array = reponses;
-		}*/
-		if(array != null){
-			Object[] temp = new Object[array.length*2];
-			for(int i =0;i<array.length;i++){
-				temp[i] = array[i];
-			}
-			if(tst instanceof Domaine){
-				domaines = (Domaine[]) temp;
-			}/*else if(tst  instanceof Question){
-				questions = (Question[]) temp;
-			}else if(tst instanceof Reponse){
-				array = (Reponse[]) reponses;
-			}*/
-			
-			temp = null;
-			return;
-		}
-	}
-	
 	private <E> void removeElement(E element){
-		Object[] temp = null;
+		LinkedList<E> temp = null;
 		if(element instanceof Domaine){
-			temp = domaines;
-		}/*else if(element  instanceof Question){
-			temp = questions;
-		}else if(element instanceof Reponse){
-			temp = reponses;
-		}*/
+			temp = (LinkedList<E>)domaines;
+		}
 		if(temp != null){
-			for(int i= this.<E>getIndexOf(element);i!=-1 && i<temp.length-1 && temp[i] !=null;i++){
-				temp[i]= temp[i+1];
-				temp[i+1] = null;
-			}
-			    temp[temp.length-1] = null;
+			temp.remove(element);
 		}
 	}
 	
 	private <E> int getIndexOf(E element){
-		Object[] temp = null;
+		LinkedList<E> temp = null;
 		if(element instanceof Domaine){
-			temp = domaines;
-		}/*else if(element  instanceof Question){
-			temp = questions;
-		}else if(element instanceof Reponse){
-			temp = reponses;
-		}*/
+			temp = (LinkedList<E>)domaines;
+		}
 		if(temp !=null){
-			for(int i = 0;i<temp.length && temp[i] !=null;i++){
-				if(element.equals(temp[i])){
-					return i;
-				}
-			}
+			temp.indexOf(element);
 		}
 		return -1;
 		
 	}
 	
 	private <E> boolean hasElement(E element){
-		Object[] temp = null;
+		LinkedList<E> temp = null;
 		if(element instanceof Domaine){
-			temp = domaines;
-		}/*else if(element instanceof Question){
-			temp = questions;
-		}else if(element instanceof Reponse){
-			temp = reponses;
-		}*/
-		if(temp !=null){
-		for(int i=0;temp[i]!=null &&i<temp.length;i++){
-			if(element.equals(domaines[i])){
-				return true;
-			}
+			temp = (LinkedList<E>)domaines;
 		}
+		if(temp !=null){
+			temp.contains(element);
 		}
 		
 		return false;
 	}
 	
 	private <E> E getElement(int index,E test){
-		Object[] temp = null;
+		LinkedList<E> temp = null;
 		if(test instanceof Domaine){
-			temp = domaines;
-		}/*else if(test  instanceof Question){
-			temp = questions;
-		}else if(test instanceof Reponse){
-			temp = reponses;
-		}*/
+			temp = (LinkedList<E>)domaines;
+		}
 		if(temp != null){
-			if(index>0 && index< temp.length-1){
-				if(test instanceof Domaine){
-					return (E)(new Domaine(domaines[index].getNom()));
-				}/*else if(test instanceof Question){
-					return (E)((Question)(temp[index])).deepCopy();
-				}else if(test instanceof Reponse){
-					return (E)((Question)(temp[index])).deepCopy();
-				}*/else{
-					return null;
-				}
+			if(index>0 && index< temp.size()-1){
+				return temp.get(index);
 			}else{
 				throw new IllegalArgumentException();
 			}
@@ -238,56 +125,28 @@ public class Utilisateur {
 	}
 	
 	private <E>void addElement(E element){
-		Object[] temp = null;
+		LinkedList<E> temp = null;
 		if(element instanceof Domaine){
-			temp = domaines;
-		}/*else if(element instanceof Question){
-			temp = questions;
-		}else if(element instanceof Reponse){
-			temp = reponses;
-		}*/
+			temp = (LinkedList<E>)domaines;
+		}
 		if(temp !=null){
-			int i;
-			for(i=0;i<temp.length;i++){
-				if(temp[i] ==null){
-					temp[i] = element;
-					break;
-				}
-			}
-			
-			if(i == temp.length){
-				this.<E>makeBiggerArray(element);
-				temp[i] = element;
-			}
+			temp.add(element);
 			return;
 		}
 	}
 	
 	private <E> E[] getElements(E test){
-		Object[] temp = null;
+		LinkedList<E> temp = null;
 		if(test instanceof Domaine){
-			temp = domaines;
-		}/*else if(test instanceof Question){
-			temp = questions;
-		}else if(test instanceof Reponse){
-			temp = reponses;
-		}*/
+			temp = (LinkedList<E>)domaines;
+		}
 		if(temp !=null){
-			if(test instanceof Domaine){
-				for(int i=0;domaines[i]!=null &&i<domaines.length;i++){
-					temp[i] = (E)new Domaine(domaines[i].getNom());
-				}
-			}/*else if(test instanceof Question){
-				for(int i=0;temp[i]!=null &&i<temp.length;i++){
-					temp[i] = (E)(((Question)(temp[i])).deepCopy());
-				}
-			} else if(test instanceof Reponse){
-				for(int i=0;temp[i]!=null &&i<temp.length;i++){
-					temp[i] = (E)(((Reponse)(temp[i])).deepCopy());
-				}
-			}*/
+			Object[] ret = new Object[temp.size()];
+			for(int i=0;i<temp.size();i++){
+				ret[i] = temp.get(i);
+			}
 			
-			return (E[])temp;
+			return (E[])ret;
 		}
 		return null;
 	}
